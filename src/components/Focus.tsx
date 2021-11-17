a0177e03265963199bb0548a5d348ab9d6828807
Zilde <sabine.zilde@accenture.com>
2021-11-17 09:37:24 +0200

    Adding BrowserRouter

---
+++ src/components/Focus.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Focus() {
+    return (
+        <div>
+            Focus view
+        </div>
+    )
+}
+
+export default Focus
---
+++ src/components/Home.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Home() {
+    return (
+        <div>
+            List view
+        </div>
+    )
+}
+
+export default Home
