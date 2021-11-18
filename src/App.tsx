cc1996c13d20a2a767230aab68282518934f2d01
Zilde <sabine.zilde@accenture.com>
2021-11-18 17:09:17 +0200

    feat: FocusScreen comp added

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,20 @@
 import React from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Focus from './components/Focus';
-import ListScreen  from './screens/ListScreen'
+import { BrowserRouter, Route, Routes, NavLink } from 'react-router-dom';
+import FocusScreen from './screens/FocusScreen';
+import ListScreen from './screens/ListScreen'
 
 function App() {
   return (
     <BrowserRouter>
+      <nav>
+        <NavLink to='/'>List</NavLink>
+        {' '}-{' '}
+        <NavLink to='focus'>Focus</NavLink>
+      </nav>
+      <br />
       <Routes>
         <Route path='/' element={<ListScreen />} />
-        <Route path='focus' element={<Focus />} />
+        <Route path='focus' element={<FocusScreen />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ src/screens/FocusScreen.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+type Props = {};
+
+const FocusScreen: React.FC<Props> = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default FocusScreen;
